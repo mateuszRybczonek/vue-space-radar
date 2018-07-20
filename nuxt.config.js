6d59d1c991a9a7fff62bbd6841693a66dc9db059
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-07-20 19:15:28 +0200

    add zircle

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,7 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~plugins/zircle.js'
   ],
 
   /*
@@ -67,5 +68,5 @@ module.exports = {
         })
       }
     }
-  }
+  },
 }

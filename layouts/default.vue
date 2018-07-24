43d0e55a9284f75042d974eb48f9a74e8087bf9e
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-07-24 19:25:13 +0200

    add GalaxyBackground to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
+    <GalaxyBackground />
     <nuxt/>
   </div>
 </template>
+
+<script>
+import GalaxyBackground from '@/components/GalaxyBackground.vue'
+
+export default {
+  components: {
+    GalaxyBackground
+  }
+}
+</script>
+
 
 <style>
 html
@@ -20,34 +32,5 @@ html
 {
   box-sizing: border-box;
   margin: 0;
-}
-.button--green
-{
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-.button--green:hover
-{
-  color: #fff;
-  background-color: #3b8070;
-}
-.button--grey
-{
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-.button--grey:hover
-{
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,29 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
+    <div class="content">
       <h1 class="title">
         vue-space-radar
       </h1>
       <h2 class="subtitle">
-        App for visualizing Netguru How We Roll when it comes to Vue.js
+        Explore Netguru / Vue.js universe
       </h2>
       <div class="links">
-        <nuxt-link class="button--grey" to="/radar">Space Radar</nuxt-link>
+        <button class="button--space">
+          <nuxt-link to="/radar">Start your journey</nuxt-link>
+        </button>
       </div>
     </div>
   </section>
 </template>
 
-<style>
+<style lang="scss">
 .container {
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
+  background-color: $color-black;
 }
 
 .title {
@@ -29,19 +31,63 @@
   display: block;
   font-weight: 300;
   font-size: 100px;
-  color: #35495e;
+  color: $color-white;
   letter-spacing: 1px;
+}
+
+.content {
+    z-index: 1;
 }
 
 .subtitle {
   font-weight: 300;
   font-size: 42px;
-  color: #526488;
+  color: $color-white;
   word-spacing: 5px;
   padding-bottom: 15px;
 }
 
 .links {
   padding-top: 15px;
+  display: flex;
+  justify-content: center;
+}
+
+.button {
+  position: relative;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  text-transform: uppercase;
+  
+  &--space {
+    height: 200px;
+    width: 200px;;
+    letter-spacing: 1px;
+    border-radius: 50%;
+    transition: all 0.6s ease-in-out;
+    outline: none;
+    box-shadow: 0 8px 16px 0 rgba(83, 99, 115, 0.32);
+    background-color: $color-black;
+    border: solid 1px $color-white;
+
+    &:hover {
+      background-color: $color-white;
+      color: $color-black;
+      cursor: pointer;
+    }
+
+    > a {
+      padding: 20px;
+      color: $color-white;
+      text-decoration: none;
+      font-size: 16px;
+
+      &:hover {
+      color: $color-black;
+    }
+    }
+  }
 }
 </style>

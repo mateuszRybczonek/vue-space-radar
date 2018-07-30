b6ec91ef2d3fb9a1148f9f0a4a0c82cb989a57f4
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-07-30 10:22:20 +0200

    move ButtonRipple to separate component

--- pages/index.vue
+++ pages/index.vue
@@ -18,50 +18,20 @@
       </div>
 
       <Astronaut
-        :mouseoverButton='mouseoverButton'
-        :mouseoutButton='mouseoutButton'
-        :astronautLaunched='astronautLaunched'
+        :mouseoverButton="mouseoverButton"
+        :mouseoutButton="mouseoutButton"
+        :astronautLaunched="astronautLaunched"
       />
 
       <nuxt-link
         class="start-link"
         to="/radar"
       >
-        <div class="start-link__circle-ripple">
-          <svg
-            :class="[
-              {
-                'start-link__circle-button--active': mouseoverButton
-              },
-              'start-link__circle-button'
-            ]"
-            xmlns="http://www.w3.org/2000/svg"
-            width="200"
-            height="200"
-            viewBox="0 0 200 200"
-            @mouseover="mouseoverButtonAction"
-            @mouseout="mouseoutButtonAction"
-            @click="launchAstronaut"
-          >
-            <transition-group tag="g">
-              <circle
-                class="circle-button__core"
-                key="core"
-                cx="8"
-                cy="8"
-                r="8"
-              />
-            </transition-group>
-            <text
-              @click="launchAstronaut"
-              class="circle-button__text"
-              x="50%"
-              y="50%"
-            >
-              Start your journey
-            </text>
-          </svg>
-        </div>
+        <ButtonRipple
+          @mouseoverButton="mouseoverButtonAction"
+          @mouseoutButton="mouseoutButtonAction"
+          @clickButton="launchAstronaut"
+        />
       </nuxt-link>
     </div>
   </section>
@@ -70,19 +40,20 @@
 <script>
 import { mapState } from 'vuex'
 import Astronaut from '@/components/IndexPageAstronaut.vue'
+import ButtonRipple from '@/components/atoms/ButtonRipple.vue'
 import Heart from '@/components/SVG/Heart.vue'
 import NetguruLogo from '@/components/SVG/NetguruLogo.vue'
 
 export default {
   components: {
     Astronaut,
+    ButtonRipple,
     Heart,
     NetguruLogo,
   },
 
   data() {
     return {
-      changed: false,
       mouseoverButton: false,
       mouseoutButton: true,
       astronautLaunched: false,
@@ -162,52 +133,6 @@ export default {
       padding-top: 15px;
       display: flex;
       justify-content: center;
-
-      &__circle-ripple {
-        background-color: $color-white;
-        width: 1em;
-        height: 1em;
-        margin-top: 100px;
-        border-radius: 50%;
-        animation: ripple 1.5s linear infinite;
-      }
-
-      &__circle-button {
-        fill: $color-white;
-        stroke: $color-white;
-        transform: translate3d(-100px, -100px, 0);
-
-        .circle-button__core {
-          transition: all 0.6s ease-in-out;
-          transform: translate3d(100px, 100px, 0);
-        }
-
-        .circle-button__text {
-          font-family: "Helvetica Neue";
-          font-weight: lighter;
-          letter-spacing: 2px;
-          transition: all 0.6s ease-in-out;
-          transform: translate3d(10px, 10px, 0);
-          text-anchor: middle;
-          stroke: transparent;
-          font-size: 0;
-        }
-
-        &--active {
-          fill: $color-white;
-          stroke: $color-white;
-
-          .circle-button__core {
-            transform: translate3d(27px, 27px, 0) scale(10);
-          }
-
-          .circle-button__text {
-            font-size: 16px;
-            transform: translate3d(5px, 10px, 0);
-            stroke: $color-black;
-          }
-        }
-      }
     }
   }
 }
